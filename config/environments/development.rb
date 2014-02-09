444010c1e2b83a86747daee9084c8087650f9807
Eugene Batogov <johnbat26@gmail.com>
2014-02-09 22:51:24 +0400

    forms completed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Angularails::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.serve_static_assets = false
+  config.assets.enabled = false
 end
